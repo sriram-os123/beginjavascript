2b4ce0dc40ec4561106c12145ca8896abfef8f21
sriram os123 <118521565+sriram-os123@users.noreply.github.com>
2025-03-08 23:08:21 +0530

    Create firstjsprogram.js

---
+++ firstjsprogram.js
@@ -0,0 +1,7 @@
+alert("hello world")
+document.body.innerhtml="my first code";
+//for rounding of numbers in javascript we use this syntax
+Math.round(4.5)
+//for creating variable we use let syntax
+let number=5.7;
+console.log(number);
